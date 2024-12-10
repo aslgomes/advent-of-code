852a727ca798236f7a077ad37bd4b901229a84fe
aslgomes <aslgomes@gmail.com>
2024-12-10 00:08:51 +0000

    Adding execution times to all days

--- src/main/java/com/project/adventofcode/day01/Main.java
+++ src/main/java/com/project/adventofcode/day01/Main.java
@@ -18,8 +18,18 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Hello Day 1");
+
+        long start1 = System.currentTimeMillis();
         partOne();
+        long end1 = System.currentTimeMillis();
+        System.out.println("time: " + (end1 - start1) + " (ms)");
+
+        System.out.println();
+
+        long start2 = System.currentTimeMillis();
         partTwo();
+        long end2 = System.currentTimeMillis();
+        System.out.println("time: " + (end2 - start2) + " (ms)");
     }
 
     private static void partOne() throws IOException {
