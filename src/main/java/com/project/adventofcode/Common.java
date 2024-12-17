5dfd57e757c0aaa8b34a3b089d7e45b36971ed48
aslgomes <aslgomes@gmail.com>
2024-12-17 17:45:00 +0000

    Day16 done

--- src/main/java/com/project/adventofcode/Common.java
+++ src/main/java/com/project/adventofcode/Common.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Common {
 
-    public static char[][] loadGrid(final String inputFilePath) throws IOException {
+    public static char[][] loadCharGrid(final String inputFilePath) throws IOException {
         final List<String> lines = Files.readAllLines(Path.of(inputFilePath));
         final int rows = lines.size();
         final int cols = lines.getFirst().length();
@@ -20,14 +20,51 @@ public class Common {
         return grid;
     }
 
+    public static int[][] loadIntGrid(final String inputFilePath) throws IOException {
+        final List<String> lines = Files.readAllLines(Path.of(inputFilePath));
+        final int rows = lines.size();
+        final int cols = lines.getFirst().length();
+
+        int[][] grid = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < lines.get(i).length(); j++) {
+                grid[i][j] = Integer.parseInt(String.valueOf(lines.get(i).charAt(j)));
+            }
+        }
+
+        return grid;
+    }
+
     public static boolean withinBounds(final char[][] grid, final int x, final int y) {
         return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length;
     }
 
-    public static void printGrid(final char[][] grid) {
+    public static boolean withinBounds(final int[][] grid, final int x, final int y) {
+        return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length;
+    }
+
+    public static void printCharGrid(final char[][] grid) {
         for (char[] chars : grid) {
             for (int j = 0; j < grid[0].length; j++) {
-                System.out.print(chars[j]);
+                System.out.print(chars[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printBooleanGrid(final boolean[][] grid) {
+        for (boolean[] chars : grid) {
+            for (int j = 0; j < grid[0].length; j++) {
+                System.out.print(chars[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printIntGrid(final int[][] grid) {
+        for (int[] nums : grid) {
+            for (int j = 0; j < grid[0].length; j++) {
+                System.out.print(nums[j] + " ");
             }
             System.out.println();
         }
