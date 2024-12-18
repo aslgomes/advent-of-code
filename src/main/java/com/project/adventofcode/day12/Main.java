6d9ebf8a7d3985c9bcd97996980adf760d3a1997
aslgomes <aslgomes@gmail.com>
2024-12-18 18:08:47 +0000

    Cleanup. Previously pushed Day12/13/14 temp code by mistake

--- src/main/java/com/project/adventofcode/day12/Main.java
+++ src/main/java/com/project/adventofcode/day12/Main.java
@@ -1,20 +1,13 @@
 package com.project.adventofcode.day12;
 
-import com.project.adventofcode.Common;
-
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.List;
 
 public class Main {
 
-    private static final String INPUT_FILE_PATH = "src/main/resources/day12/smaller-input-2.txt";
-
-    private static boolean[][] processed;
+    private static final String INPUT_FILE_PATH = "src/main/resources/day12/input.txt";
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello Day 1");
+        System.out.println("Hello Day 12");
 
         long start1 = System.currentTimeMillis();
         partOne();
@@ -30,72 +23,7 @@ public class Main {
     }
 
     private static void partOne() throws IOException {
-//        final char[][] grid = Common.loadCharGrid(INPUT_FILE_PATH);
-//        int fencePrice = 0;
-//        processed = new boolean[grid.length][grid[0].length];
-//
-//        for (int i = 0; i < grid.length; i++) {
-//            for (int j = 0; j < grid[0].length; j++) {
-//                if (!processed[i][j]) {
-//                    System.out.println("processing char-" + grid[i][j]);
-//                    fencePrice += dfs(grid, i, j);
-//                }
-//            }
-//        }
-//
-//        System.out.println("fencePrice=" + fencePrice);
     }
-
-//    private static int dfs(final char[][] grid, int x, int y) {
-//        int area = 0;
-//        int perimeter = 0;
-//
-//        final Deque<int[]> stack = new ArrayDeque<>();
-//        stack.push(new int[] {x, y});
-//
-//        while (!stack.isEmpty()) {
-//            final int[] coordinates = stack.pop();
-//            final int coordinateX = coordinates[0];
-//            final int coordinateY = coordinates[1];
-//
-//            if (processed[coordinateX][coordinateY]) {
-//                continue;
-//            }
-//
-//            processed[coordinateX][coordinateY] = true;
-//            area++;
-//            perimeter += 4;
-//            System.out.println("processed grid[" + coordinateX + "][" + coordinateY + "]");
-//            System.out.println("area = " + area);
-//            System.out.println("perimeter = " + perimeter);
-//            final char curr = grid[coordinateX][coordinateY];
-//
-//            final List<int[]> neighbours = List.of(
-//                    new int[] { coordinateX + 1, coordinateY },
-//                    new int[] { coordinateX - 1, coordinateY },
-//                    new int[] { coordinateX, coordinateY + 1 },
-//                    new int[] { coordinateX, coordinateY - 1 }
-//            );
-//
-//            for (int[] neighbour: neighbours) {
-//                if (Common.withinBounds(grid, neighbour[0], neighbour[1])
-//                        && grid[neighbour[0]][neighbour[1]] == curr) {
-//
-//                    if (!processed[neighbour[0]][neighbour[1]]) {
-//                        perimeter
-//                        stack.push(new int[] { neighbour[0], neighbour[1] });
-//                    }
-//                }
-//            }
-//        }
-//
-//        System.out.println(" area=" + area);
-//        System.out.println(" perimeter=" + perimeter);
-//        System.out.println();
-//        System.out.println();
-//
-//        return area * perimeter;
-//    }
 
     private static void partTwo() throws IOException {
     }
--- src/main/java/com/project/adventofcode/day14/Main.java
+++ src/main/java/com/project/adventofcode/day14/Main.java
@@ -1,24 +1,10 @@
 package com.project.adventofcode.day14;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
 
     private static final String INPUT_FILE_PATH = "src/main/resources/day14/input.txt";
-
-    // fixed
-    private static final int ROWS = 103;
-
-    // fixed
-    private static final int COLS = 101;
 
     public static void main(String[] args) throws IOException {
         System.out.println("Hello Day 14");
@@ -37,74 +23,6 @@ public class Main {
     }
 
     private static void partOne() throws IOException {
-        final List<String> lines = Files.readAllLines(Path.of(INPUT_FILE_PATH));
-        final int[][] grid = new int[ROWS][COLS];
-        final String regex = "p=(-?\\d+),(-?\\d+)\\sv=(-?\\d+),(-?\\d+)";
-
-        final Map<String, int[]> positions = new HashMap<>();
-        final Map<String, int[]> speeds = new HashMap<>();
-
-        for (String line: lines) {
-            final Pattern pattern = Pattern.compile(regex);
-            final Matcher matcher = pattern.matcher(line);
-
-            while (matcher.find()) {
-                final String id = UUID.randomUUID().toString();
-                positions.put(id, new int[] { Integer.parseInt(matcher.group(2)), Integer.parseInt(matcher.group(1)) });
-                speeds.put(id, new int[] { Integer.parseInt(matcher.group(4)), Integer.parseInt(matcher.group(3)) });
-            }
-        }
-
-        move(grid, positions, speeds, 100);
-        System.out.println("safetyScore=" + safetyScore(grid));
-    }
-
-    private static void move(
-            final int[][] grid,
-            final Map<String, int[]> positions,
-            final Map<String, int[]> speeds,
-            int times) {
-
-        for (String key : positions.keySet()) {
-            for (int i = 0; i < times; i++) {
-                int newX = ((positions.get(key)[0] + speeds.get(key)[0]) % ROWS + ROWS) % ROWS;
-                int newY = ((positions.get(key)[1] + speeds.get(key)[1]) % COLS + COLS) % COLS;
-                positions.put(key, new int[] { newX, newY });
-            }
-            grid[positions.get(key)[0]][positions.get(key)[1]]++;
-        }
-    }
-
-    private static long safetyScore(final int[][] grid) {
-        int q1Sum = 0;
-        for (int i = 0; i < ROWS / 2; i++) {
-            for (int j = 0; j < COLS / 2; j++) {
-                q1Sum += grid[i][j];
-            }
-        }
-
-        int q2Sum = 0;
-        for (int i = 0; i < ROWS / 2; i++) {
-            for (int j = (COLS / 2 + 1); j < COLS; j++) {
-                q2Sum += grid[i][j];
-            }
-        }
-
-        int q3Sum = 0;
-        for (int i = (ROWS / 2 + 1); i < ROWS; i++) {
-            for (int j = 0; j < COLS / 2; j++) {
-                q3Sum += grid[i][j];
-            }
-        }
-
-        int q4Sum = 0;
-        for (int i = (ROWS / 2 + 1); i < ROWS; i++) {
-            for (int j = (COLS / 2 + 1); j < COLS; j++) {
-                q4Sum += grid[i][j];
-            }
-        }
-
-        return ((long) q1Sum * q2Sum * q3Sum * q4Sum);
     }
 
     private static void partTwo() throws IOException {
