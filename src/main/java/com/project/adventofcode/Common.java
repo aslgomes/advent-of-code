8fa89951b54dd4a6f57f2f5185e808afc29b477a
aslgomes <aslgomes@gmail.com>
2025-01-05 01:03:14 +0000

    Cleaning up imports

--- src/main/java/com/project/adventofcode/Common.java
+++ src/main/java/com/project/adventofcode/Common.java
@@ -1,7 +1,10 @@
 package com.project.adventofcode;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
